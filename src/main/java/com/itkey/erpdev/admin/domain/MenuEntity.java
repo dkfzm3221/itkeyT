02067b54849c05a5f57b35495fc9eb8a69ae6f0c
ljhoo0070 <126861718+ljhoo0070@users.noreply.github.com>
2023-11-23 14:09:49 +0900

    메뉴 및 게시글 역순 수정

--- src/main/java/com/itkey/erpdev/admin/domain/MenuEntity.java
+++ src/main/java/com/itkey/erpdev/admin/domain/MenuEntity.java
@@ -16,6 +16,7 @@ public class MenuEntity {
 	private String menuConnWay = "";	// 메뉴 연결방식연결방식(0 : 현재창, 1 : 새창, 2 : 팝업창)
 	private String menuParentSeq = "";	// 부모 메뉴 번호
 	private String menuBoardType = "";	// 메뉴 게시판 타입
+	private String delMenuBoardType = ""; // 삭제할 메뉴의 게시판 타입
 
 	private String regId = "";
 	private String updDt = "";
